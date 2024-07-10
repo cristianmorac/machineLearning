1e83368c657031e5f4708345ca74f5054110d7bd
cristianmorac <cristianfmorac@gmail.com>
2024-07-10 11:34:29 -0500

    feat: graficos interactivos

--- prueba.py
+++ prueba.py
@@ -1,18 +1,34 @@
-import json
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
-diccionario1 = {'nombre': 'Juan', 'edad': 30, 'ciudad': 'Madrid'}
-diccionario2 = {'nombre': 'Ana', 'edad': 25, 'ciudad': 'Barcelona'}
-arch = 'prueba.txt'
-def create_arch(dic, arch, info):
-    with open(arch, 'a') as archivo:
-        archivo.write(f"-------------------{info}-------------------\n")
-        if dic == '':
-            return
-        archivo.write(json.dumps(dic,indent=4) + '\n')
-        
+# Datos de ejemplo
+data = {
+    'columna_x': ['A', 'B', 'C', 'D'],
+    'columna_y': [10, 20, 15, 25],
+    'title': 'Título del Gráfico',
+    'xlabel': 'Eje X',
+    'ylabel': 'Eje Y'
+}
 
-create_arch(diccionario1,arch, 'Supervisado')
-create_arch(diccionario2,arch, 'No supervisado')
-create_arch('',arch, 'Cabecera')
+dataframe = pd.DataFrame(data)
 
+# Configuración de los datos del gráfico
+datos_grafica_barra = {
+    'columna_x': 'columna_x',
+    'columna_y': 'columna_y',
+    'title': data['title'],
+    'xlabel': data['xlabel'],
+    'ylabel': data['ylabel']
+}
 
+# Creación del gráfico
+plt.figure(figsize=(8, 6))
+sns.barplot(x=datos_grafica_barra['columna_x'], y=datos_grafica_barra['columna_y'], data=dataframe)
+plt.title(datos_grafica_barra['title'])
+plt.xlabel(datos_grafica_barra['xlabel'])
+plt.ylabel(datos_grafica_barra['ylabel'])
+
+# Mostrar el gráfico en Streamlit
+st.pyplot(plt)
