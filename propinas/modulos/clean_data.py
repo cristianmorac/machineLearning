3faea9763de7e0df17a704d8649501c0beff02c7
Cristian Mora <cristianfmorac@gmail.com>
2024-07-18 03:31:30 -0500

    feat:archivos proyecto final

--- propinas/modulos/clean_data.py
+++
@@ -1,7 +0,0 @@
-def limpiar_datos(df, dic):
-
-    # obtener todas las columnas
-    columnas = df.columns.to_list()
-
-    df[columnas] = df[columnas].replace(dic , regex=True)
-    return df
--- propinas/modulos/datos.py
+++
@@ -1,5 +0,0 @@
-replace_letra = {
-    'Ã¡': 'a', # cambiada
-    'Ã©': 'e', # cambiada
-    'Ã³': 'o'
-}
